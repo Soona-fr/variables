2f7e136eb3f37517f2f8480c42e1694ca34903fa
Soona-fr <il.ovechka@gmail.com>
2024-11-19 23:26:31 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+# variables
+#---------------------------
+compl_tasks = 12
+num_hours_spent= 1.5
+cour_name = 'Python'
+t_1_task = (num_hours_spent / compl_tasks)
+#---------------------------
+print('Курс:', cour_name,',', 'всего задач:', compl_tasks,',', 'затрачено часов:', num_hours_spent,',', 'среднее время выполнения:', t_1_task)
